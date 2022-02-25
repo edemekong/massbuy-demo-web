a42527ace04361808c6771a8a164de90d711c07b
Paul Edemekong <hello.paulvin@gmail.com>
2022-02-25 21:48:12 +0100

    v3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "554e51750cf77b7ac971e34abe730b0b",
-"index.html": "ede10d1df2acb872827b38efaf7a52d0",
-"/": "ede10d1df2acb872827b38efaf7a52d0",
-"main.dart.js": "508bf154b0f5c79d5fd9a925dbd284a3",
+"index.html": "f8db077132ff5237a3d9aa0211db8dd0",
+"/": "f8db077132ff5237a3d9aa0211db8dd0",
+"main.dart.js": "306179e4484c40598aea51fd371adeab",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
